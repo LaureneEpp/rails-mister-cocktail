3e77f7ca0af32ba69c91fc8e18ecef8166e066b9
Laurene Eppelle <laurene.eppelle@gmail.com>
2020-12-19 12:24:44 +0100

    Add Description to Doses

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -2,8 +2,6 @@ class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
 
-  has_one :cocktail, :ingredient
-
   validates :description, presence: true
   validates :cocktail, presence: true
   validates :ingredient, presence: true
