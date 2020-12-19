41b130c2e135b9b15f8ffa750ce246437d017220
Laurene Eppelle <laurene.eppelle@gmail.com>
2020-12-19 11:41:12 +0100

    Add cocktails controller

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,2 +1,7 @@
 class Cocktail < ApplicationRecord
+  has_many :doses
+  validates_associated :doses
+
+
+  validates :name, presence: true, uniqueness: true
 end
--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,4 +1,10 @@
 class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
+
+  has_one :cocktail, :ingredient
+
+  validates :description, presence: true
+  validates :cocktail, presence: true
+  validates :ingredient, presence: true
 end
